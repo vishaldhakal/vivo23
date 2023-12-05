2f0449573840988a38bd9f5fce85ca4ecc0050d9
vishaldhakal <vishaldhakal96@gmail.com>
2023-12-05 22:50:12 +0545

    dfv

--- offer/settings.py
+++ offer/settings.py
@@ -12,7 +12,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
-CSRF_TRUSTED_ORIGINS = ['http://localhost:8000', 'https://vivodashain.com','http://127.0.0.1:8000']
+CSRF_TRUSTED_ORIGINS = ['http://localhost:8000', 'https://vivocarnival.com','http://127.0.0.1:8000']
 
 
 # Application definition
