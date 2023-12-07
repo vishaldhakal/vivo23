702b59753307db40fd5221e8b91bf7c516a68307
vishaldhakal <vishaldhakal96@gmail.com>
2023-12-07 08:38:56 +0545

    'dsfv

--- offer/settings.py
+++ offer/settings.py
@@ -122,7 +122,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     
 ]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = Path(BASE_DIR, 'media')
