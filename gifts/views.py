7a54a95d15ba893d6a5c1f25f9598b6cbb4e9427
vishaldhakal <vishaldhakal96@gmail.com>
2023-12-13 08:07:40 +0545

    DFv

--- gifts/views.py
+++ gifts/views.py
@@ -483,7 +483,7 @@ def registerCustomer(request):
         
         imeiii = IMEINO.objects.get(imei_no=imei_number)
         
-        phone_model = imeei.phone_model
+        phone_model = imeiii.phone_model
 
         # Create a new customer
         customer = Customer.objects.create(
@@ -556,7 +556,7 @@ def registerCustomer(request):
                 #also check first letter of phone_model if its Y or V
                 c1 = offer.validto[0]
                 c2 = offer.validto
-                if ((get_sale_count + 1) in offer.sale_numbers) and (offer.quantity > 0) and phone_model!="Y27s(8+256G)_EX":
+                if ((get_sale_count + 1) in offer.sale_numbers) and (offer.quantity > 0) and (phone_model!="Y27s(8+256G)_EX"):
                     if phone_model[0] == c1 or c2 == "All":
                         qty = offer.quantity
                         customer.gift = offer.gift
@@ -571,7 +571,7 @@ def registerCustomer(request):
                 c1 = offer.validto[0]
                 c2 = offer.validto
                 if offer.type_of_offer == "After every certain sale":
-                    if (((get_sale_count + 1) % int(offer.offer_condition_value) == 0)) and (offer.quantity > 0)  and phone_model!="Y27s(8+256G)_EX":
+                    if (((get_sale_count + 1) % int(offer.offer_condition_value) == 0)) and (offer.quantity > 0)  and (phone_model!="Y27s(8+256G)_EX"):
                         if phone_model[0] == c1 or c2 == "All":
                             qty = offer.quantity
                             customer.gift = offer.gift
@@ -581,7 +581,7 @@ def registerCustomer(request):
                             gift_assigned = True
                             break
                 if offer.type_of_offer == "At certain sale position":
-                    if ((get_sale_count + 1) == int(offer.offer_condition_value)) and (offer.quantity > 0)  and phone_model!="Y27s(8+256G)_EX":
+                    if ((get_sale_count + 1) == int(offer.offer_condition_value)) and ((offer.quantity > 0))  and (phone_model!="Y27s(8+256G)_EX"):
                         if phone_model[0] == c1 or c2 == "All":
                             qty = offer.quantity
                             customer.gift = offer.gift
